9da2535b4ba0740731cac8205c7bd595689a2962
fabiensen <fabio@Fabios-Mac-Pro.station>
2016-11-21 15:01:46 +0100

    a test for php
    Just a description

---
+++ employees_lang.php
@@ -0,0 +1,27 @@
+<?php
+$lang['employees_employee'] = 'Impiegato';
+$lang['employees_new'] = 'Nuovo Impiegato';
+$lang['employees_update'] = 'Modifica Impiegato';
+$lang['employees_confirm_delete'] = 'Sei sicuro di voler rimuovere l\'impiegato selezionato?';
+$lang['employees_none_selected'] = 'Non hai selezionato alcun impiegato da rimuovere';
+$lang['employees_error_adding_updating'] = 'Error adding/updating employee';
+$lang['employees_successful_adding'] = 'Hai aggiunto un nuovo impiegato';
+$lang['employees_successful_updating'] = 'Hai modificato un impegato con successo';
+$lang['employees_successful_deleted'] = 'Hai rimosso uno o più';
+$lang['employees_one_or_multiple'] = 'impiegati';
+$lang['employees_cannot_be_deleted'] = 'Non ho potuto rimouvere gli impiegati selezionati, stai forse tentando di cancellare te stesso?';
+$lang['employees_username'] = 'Username';
+$lang['employees_password'] = 'Password';
+$lang['employees_repeat_password'] = 'Ripeti Passwors';
+$lang['employees_username_required'] = 'Lo username è un valore unico';
+$lang['employees_username_minlength'] = 'Lo username deve essere lungo almeno 5 caratteri';
+$lang['employees_password_required'] = 'La password è richiesta';
+$lang['employees_password_minlength'] = 'La password deve essere lunga almeno 8 caratteri';
+$lang['employees_password_must_match'] = 'Le password non corrispondono';
+$lang['employees_basic_information'] = 'Informazioni base dell\'impiegato';
+$lang['employees_login_info'] = 'Informazioni di accesso dell\'impiegato';
+$lang['employees_permission_info'] = 'Permessi dell\'impiegato';
+$lang['employees_permission_desc'] = 'Spunta le caeelle in basso per garantire accesso ai moduli';
+$lang['employees_error_updating_demo_admin'] = 'Non puoi cambiare l\'account demo';
+$lang['employees_error_deleting_demo_admin'] = 'Non puoi cancellare l\'account demo';
+?>
